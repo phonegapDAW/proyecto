55189d75efc8f96f807e674eb7edbe84761558a5
phonegapDAW <loonyworks@gmail.com>
2015-05-10 19:01:08 +0200

    Últimos retoques Estructura
    Estructura pulida, bien documentada y sencilla de modificar.

--- out/production/android/js/index.js
+++ out/production/android/js/index.js
@@ -1,12 +1,12 @@
- $(document).ready(function() {
+$(document).ready(function() {
     $.ajax({
         url:   'http://noticiasprogramacion.esy.es/damerss.php',
         type:  'post',
         beforeSend: function () {
-                $("#rss").html("<center>Procesando, espere por favor...</center>");
+            $("#rss").html("<center>Procesando, espere por favor...</center>");
         },
         success:  function (response) {
-                $("#rss").html(response);
+            $("#rss").html(response);
         }
     });
 });
